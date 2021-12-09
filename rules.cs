45199beb745fdaca899e58de3d5512f334ee7952
Jaclyn Ngo <73602536+orange-flower@users.noreply.github.com>
2021-12-09 07:40:23 -0600

    Add files via upload

---
+++ rules.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class rules : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button rulesButton;
+    public Button tutorialButton;
+    public Button readyButton;
+
+    public Animator rulesAnimator;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //plays open rules anim
+    public void openRules()
+    {
+        rulesAnimator.SetBool("openRules", true);
+    }
+
+    //closes rules anim
+    public void closeRules()
+    {
+        rulesAnimator.SetBool("openRules", false);
+    }
+}
