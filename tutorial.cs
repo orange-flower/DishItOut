3350b3d06a9a0908e7c2b09436202ad4b7f11bfb
Jaclyn Ngo <73602536+orange-flower@users.noreply.github.com>
2021-12-09 07:48:12 -0600

    Add files via upload

---
+++ tutorial.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tutorial : MonoBehaviour
+{
+    [Header("Video refs")]
+    public GameObject vid1;
+    public GameObject vid2;
+    public GameObject vid3;
+    public GameObject vid4;
+    public GameObject vid5;
+    public Button tutorialButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //setup tutorial with all vids off
+        vid1.SetActive(false);
+        vid2.SetActive(false);
+        vid3.SetActive(false);
+        vid4.SetActive(false);
+        vid5.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //functions for playing vids
+    public void startVid()
+    {
+        vid1.SetActive(false);
+        vid1.SetActive(true);
+    }
+    public void playVid2()
+    {
+        vid1.SetActive(false);
+        vid2.SetActive(false);
+        vid2.SetActive(true);
+    }
+    public void playVid3()
+    {
+        vid2.SetActive(false);
+        vid3.SetActive(false);
+        vid3.SetActive(true);
+    }
+    public void playVid4()
+    {
+        vid3.SetActive(false);
+        vid4.SetActive(false);
+        vid4.SetActive(true);
+    }
+    public void playVid5()
+    {
+        vid4.SetActive(false);
+        vid5.SetActive(false);
+        vid5.SetActive(true);
+    }
+
+    public void endTutorial()
+    {
+        vid5.SetActive(false);
+    }
+}
